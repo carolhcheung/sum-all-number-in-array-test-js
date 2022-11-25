2e012a5f62a8a62d7f8dc86ab2531affe7022a93
Carol Cheung <carol.cheung@telus.com>
2022-11-24 22:34:41 -0800

    sum numbers in nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function sumItems(array) {
+  newArr = array.flat(Infinity)
+  result = 0;
+  for (i of newArr) {
+    result += i;
+  }
+  return result;
   // Sum all the numbers in the array
 }
-
 module.exports = sumItems;
